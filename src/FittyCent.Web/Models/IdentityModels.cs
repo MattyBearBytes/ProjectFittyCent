815b93d4a42d85bcbbc80ddad284728ec1b41a56
Marc Armstrong <marc.armstrong@outlook.com>
2014-02-22 11:31:17 +0800

    add core projects

--- src/FittyCent.Web/Models/IdentityModels.cs
+++ src/FittyCent.Web/Models/IdentityModels.cs
@@ -10,7 +10,7 @@ namespace FittyCent.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection")
+            : base("projectfittycent")
         {
         }
     }
