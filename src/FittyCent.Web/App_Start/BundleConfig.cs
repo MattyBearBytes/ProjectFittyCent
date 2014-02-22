98d5b3869981d01a043f00e4847383e9ad595788
Victor Tsen <vtsen@brighttechnologies.com.au>
2014-02-22 12:35:19 +0800

    Change bootstrap theme to light green and include it in the bundle.

--- src/FittyCent.Web/App_Start/BundleConfig.cs
+++ src/FittyCent.Web/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace FittyCent.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/normalize.css",
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
