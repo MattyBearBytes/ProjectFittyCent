06b89513b444ea24a6185ddcb720c804f26bcd3d
Matt <matt@thebearbytes.com.au>
2014-02-22 21:56:08 +0800

    Image on register page

---
+++ src/FittyCent.Web/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace FittyCent.Web.Controllers 
+{
+    public class RegisterController : Controller 
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("Register", "Account");
+        }
+    }
+}
