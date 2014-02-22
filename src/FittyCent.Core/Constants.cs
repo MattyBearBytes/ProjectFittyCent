e4504b31d37870f3031acb35273dc9c3eb9577e1
Marc Armstrong <marc.armstrong@outlook.com>
2014-02-22 23:58:30 +0800

    implement trainer class creation

--- src/FittyCent.Core/Constants.cs
+++ src/FittyCent.Core/Constants.cs
@@ -1,13 +1,27 @@
 ﻿namespace FittyCent {
     public static class Constants {
-        public static class Roles
-        {
+        public static class Roles {
             public const string Trainer = "Trainer";
         }
 
-        public static class Claims
-        {
+        public static class Claims {
             public const string Role = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role";
         }
+
+        public static string[] ClassTypes =
+        {
+            "Bootcamp",
+            "Boxing",
+            "Crossfit",
+            "Core Training",
+            "Mums & Bubs",
+            "Kids Classes",
+            "Over 50s",
+            "Running/Walking",
+            "Sporting",
+            "Tai Chi",
+            "Teens Classes",
+            "Yoga"
+        };
     }
 }
