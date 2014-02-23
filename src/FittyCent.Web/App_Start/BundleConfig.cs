bb1bd9fa9f6382b7ea956716fdd8f7714ee0ff9d
Marc Armstrong <marc.armstrong@outlook.com>
2014-02-23 15:51:40 +0800

    fix missing js files

--- src/FittyCent.Web/App_Start/BundleConfig.cs
+++ src/FittyCent.Web/App_Start/BundleConfig.cs
@@ -1,12 +1,9 @@
 ﻿using System.Web.Optimization;
 
-namespace FittyCent.Web
-{
-    public class BundleConfig
-    {
+namespace FittyCent.Web {
+    public class BundleConfig {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles)
-        {
+        public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
