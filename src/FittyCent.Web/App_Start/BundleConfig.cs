a6ef18fb8941ce34ef1d8d7252e7378cf6e6cc7e
Matt <matt@thebearbytes.com.au>
2014-03-10 23:20:30 +0800

    Sexy up home/about/contact.

--- src/FittyCent.Web/App_Start/BundleConfig.cs
+++ src/FittyCent.Web/App_Start/BundleConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System.Web.Optimization;
 
-namespace FittyCent.Web {
-    public class BundleConfig {
+namespace FittyCent.Web 
+{
+    public class BundleConfig 
+    {
         public static void RegisterBundles(BundleCollection bundles) 
         {
                 RegisterStyleBundles(bundles);
@@ -10,21 +12,31 @@ namespace FittyCent.Web {
 
         public static void RegisterStyleBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                 "~/Content/bootstrap/flatly-css/bootstrap.css",
-                 "~/Content/app/css/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/bootstrap/flatly-css/bootstrap.css")
+                .Include("~/Content/app/css/site.css"));
 
-            bundles.Add(new StyleBundle("~/Content/css/login").Include(
-                "~/Content/app/css/login.css"));  
+            bundles.Add(new StyleBundle("~/Content/css/login")
+                .Include("~/Content/app/css/login.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css/about")
+                .Include("~/Content/app/css/about.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css/onepage")
+                .Include("~/Content/onepage/onepage-scroll.css"));
         }
 
         public static void RegisterScriptBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
+                .Include("~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/onepage")
+                .Include("~/Content/onepage/jquery.onepage-scroll.js"));
+
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap")
                 .Include("~/Content/bootstrap/js/bootstrap.js")
